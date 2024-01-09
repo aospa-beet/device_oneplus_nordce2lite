499d9c6f73b711b73f73217ba68e215780c410aa
Sugakesshaa <sugakesshaa@pixelos.net>
2024-01-09 23:43:41 +0800

    nordce2lite: Initial rebrand to OnePlus Nord CE 2 Lite
    Change-Id: I28cdc2e6412dc4cfc694fbee0f45429f4873b27a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE_PATH := device/oneplus/oneplus9
+DEVICE_PATH := device/oneplus/nordce2lite
 
 include build/make/target/board/BoardConfigMainlineCommon.mk
 
@@ -80,7 +80,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 TARGET_SCREEN_DENSITY := 480
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := lahaina
+TARGET_BOOTLOADER_BOARD_NAME := holi
 
 # DTB
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
@@ -125,7 +125,7 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_BINARIES := kernel
 BOARD_KERNEL_PAGESIZE := 4096
-KERNEL_DEFCONFIG := vendor/lahaina-qgki_defconfig
+KERNEL_DEFCONFIG := vendor/holi-qgki_defconfig
 USE_KERNEL_AOSP_LLVM := true
 KERNEL_FULL_LLVM := true
 
@@ -133,7 +133,7 @@ KERNEL_FULL_LLVM := true
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/chg_enable
 
 # OTA
-TARGET_OTA_ASSERT_DEVICE := OnePlus9|oneplus9|OnePlus9Pro|oneplus9pro
+TARGET_OTA_ASSERT_DEVICE := OP535DL1|nordce2lite
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0xC000000
@@ -178,7 +178,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Touch
-TARGET_POWER_FEATURE_EXT_LIB := //$(DEVICE_PATH):libpowerfeature_ext_oneplus9
+TARGET_POWER_FEATURE_EXT_LIB := //$(DEVICE_PATH):libpowerfeature_ext_nordce2lite
 
 # Vibrator
 SOONG_CONFIG_NAMESPACES += OPLUS_AOSPA_VIBRATOR_HAL
