63111d3b8e6bca62a3b97455c04955379df513f2
Sugakesshaa <sugakesshaa@pixelos.net>
2024-01-10 19:48:05 +0800

    nordce2lite: Don't build udfps dependencies
    Change-Id: I4f8bc43e83edf34184dbcded1eb86f472c5e0874

--- device.mk
+++ device.mk
@@ -60,7 +60,6 @@ PRODUCT_PACKAGES += \
     libstdc++_vendor
 
 # Biometrics
-TARGET_USES_FOD_ZPOS := true
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
@@ -158,7 +157,6 @@ PRODUCT_PACKAGES += \
     OnePlus9SeriesFrameworks \
     OnePlus9SeriesSettings \
     OnePlus9SeriesSystemUI \
-    OnePlus9Settings \
     OnePlus9SettingsProvider \
     OnePlus9SystemUI
 
@@ -211,13 +209,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
-PRODUCT_COPY_FILES += \
-    hardware/oplus/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.nordce2lite-multihal \
-    libsensorndkbridge \
-    sensors.oplus
+    libsensorndkbridge
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
