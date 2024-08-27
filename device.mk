94efa3b1e6726b7a59bbcee69e2b6ef6a0f11200
Sugakesshaa <sugakesshaa@gmail.com>
2024-08-28 01:29:20 +0800

    Revert "nordce2lite: Import perfboostsconfig.xml from stock"
    This reverts commit 6f78030b535af3c251b4a93407ddfe3a2c8179b3.

--- device.mk
+++ device.mk
@@ -143,7 +143,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Performance
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Platform
